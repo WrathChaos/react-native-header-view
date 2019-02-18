8266b8c1253cc7cba704df6817a7cd608cf0ddda
FreakyCoder <kurayogun@gmail.com>
2019-02-18 18:27:44 +0300

    Example file is added.

---
+++ examples/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  StyleSheet, 
+  Linking,
+  Text, 
+  View, 
+  TouchableOpacity
+} from 'react-native';
+import Header from '@freakycoder/react-native-header-view';
+import Icon from "react-native-dynamic-vector-icons"
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header
+          headerTitle="Header"
+          rightComponent={
+            <TouchableOpacity
+              style={{ right: 8 }}
+              onPress={() => Linking.openURL('https://github.com/WrathChaos')} >
+              <Icon name="github" type="AntDesign" size={30} color="purple" />
+            </TouchableOpacity>
+          }
+        />
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
