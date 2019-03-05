41e52c7996e326107952c7cc50f0a032dd4d02be
FreakyCoder <kurayogun@gmail.com>
2019-03-05 15:58:41 +0300

    New version : 0.2.2: Now fully functional with Android and iOS. New feature: statusBarHidden; it re-arrange itself if you do not use status bar or not.

--- examples/App.js
+++ examples/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React from "react";
 import {
+  StatusBar,
   StyleSheet,
   Linking,
   Text,
   View,
   TouchableOpacity
-} from 'react-native';
-import Header from '@freakycoder/react-native-header-view';
-import Icon from "react-native-dynamic-vector-icons"
+} from "react-native";
+import Header from "@freakycoder/react-native-header-view";
+import Icon from "react-native-dynamic-vector-icons";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar hidden />
         <Header
+          statusBarHidden
           headerTitle="Header"
           rightComponent={
             <TouchableOpacity
               style={{ right: 8 }}
-              onPress={() => Linking.openURL('https://github.com/WrathChaos')} >
+              onPress={() => Linking.openURL("https://github.com/WrathChaos")}
+            >
               <Icon name="github" type="AntDesign" size={30} color="purple" />
             </TouchableOpacity>
           }
         />
-        <Text style={{
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}>
-          Open up App.js to start working on your app!
+        <View style={styles.textContainer}>
+          <Text style={styles.textStyle}>
+            Open up App.js to start working on your app!
           </Text>
+        </View>
       </View>
     );
   }
@@ -37,6 +40,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
+  textContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textStyle: {
+    textAlign: "center"
+  }
 });
