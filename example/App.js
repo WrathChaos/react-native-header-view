28f291f77534081b762b59141e6a2a0e92137a28
FreakyCoder <kurayogun@gmail.com>
2019-08-11 17:37:15 +0300

    ClassicHeader is working with the new refactoring

---
+++ example/App.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+import { StatusBar, SafeAreaView, TouchableOpacity } from "react-native";
+import { ClassicHeader, ModernHeader } from "./lib/src/index";
+import Icon from "react-native-dynamic-vector-icons";
+
+const App = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ClassicHeader
+          statusBarHidden
+          headerTitle="Header"
+          rightComponent={
+            <TouchableOpacity
+              style={{ right: 12 }}
+              onPress={() => Linking.openURL("https://github.com/WrathChaos")}
+            >
+              <Icon name="github" type="AntDesign" size={30} color="purple" />
+            </TouchableOpacity>
+          }
+        />
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ example/lib/src/index.js
@@ -0,0 +1,4 @@
+import ClassicHeader from "./components/ClassicHeader/ClassicHeader";
+import ModernHeader from "./components/ModernHeader/ModernHeader";
+
+export { ClassicHeader, ModernHeader };
